06dc796e26cd7b414c8865808d0ea3c357decf40
mwoodward <mwoodward@gatech.edu>
2025-08-12 13:43:25 -0700

    Add better mcap conversion help text

--- db3_to_mcap/convert.sh
+++ db3_to_mcap/convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = '-h' ] || [ "$1" = '--help' ]; then
-	echo "Usage: convert.sh <path_to_db3_file> will produce an mcap file in ros_output"
+	printf "Usage: convert.sh <path_to_db3_file> will produce an mcap file in ros_output.\nmcap_conversion.yaml specifies config options such as specific topics to convert\n";
 else
 	ros2 bag convert -i $1 -o mcap_conversion.yaml;
 fi
